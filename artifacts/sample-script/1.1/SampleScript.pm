b769d8a7f1aa38dd113bc7e31e4959ef9c56d39e
psilabs-dev <113860476+psilabs-dev@users.noreply.github.com>
2026-04-24 21:39:39 -0700

    mock update v1.1

---
+++ artifacts/sample-script/1.1/SampleScript.pm
@@ -0,0 +1,33 @@
+package LANraragi::Plugin::Managed::Scripts::SampleScript;
+
+use strict;
+use warnings;
+no warnings 'uninitialized';
+
+# Meta-information about your plugin.
+sub plugin_info {
+
+    return (
+        # Standard metadata
+        name        => "Sample Script",
+        type        => "script",
+        namespace   => "sample-script",
+        author      => "koyomi",
+        version     => "1.1",
+        description => "Script example",
+        oneshot_arg => "Value to echo back"
+    );
+
+}
+
+# Mandatory function to be implemented by your script
+sub run_script {
+    shift;
+    my $lrr_info = shift;
+
+    my $arg = $lrr_info->{oneshot_param};
+
+    return ( result => "v1.1: " . ( $arg // "no argument provided" ) );
+}
+
+1;
---
+++ artifacts/title-suffix-2/1.1/TitleSuffix2.pm
@@ -0,0 +1,37 @@
+package LANraragi::Plugin::Managed::Metadata::TitleSuffix2;
+
+use strict;
+use warnings;
+no warnings 'uninitialized';
+
+use LANraragi::Model::Plugins;
+
+# Meta-information about your plugin.
+sub plugin_info {
+
+    return (
+        # Standard metadata
+        name        => "Title Suffix 2",
+        type        => "metadata",
+        namespace   => "title-suffix-2",
+        author      => "koyomi",
+        version     => "1.1",
+        description => "Adds a '-2' suffix to the current title",
+        parameters  => [],
+        oneshot_arg => "Optional tag to add"
+    );
+
+}
+
+# Mandatory function to be implemented by your metadata plugin
+sub get_tags {
+
+    shift;
+    my $lrr_info = shift;
+
+    my $title = $lrr_info->{archive_title} // "";
+
+    return ( tags => "", title => "$title-2" );
+}
+
+1;
